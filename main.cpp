31e51cd8f4c4ff599bc67eea729e1d6f10735f2f
Gallay David <david.gallay.96@gmail.com>
2020-05-28 23:09:11 +0200

    Change main.cpp to be useful, Should add tests

--- main.cpp
+++ main.cpp
@@ -5,32 +5,49 @@
 
 
 int main(int argc, char *argv[]) {
-    if(argc < 5) return EXIT_FAILURE;
-    std::cout << "Right number of parameters" << std::endl;
+    if(argc < 7){
+        std::cerr << "Parameters: URL DATABASE LOGIN PASSWORD UID MODEL METHOD [ARGS]" << std::endl;
+        return EXIT_FAILURE;
+    }
 
-    const std::string URL = argv[1];
-    const std::string DATABASE = argv[2];
+    const char* URL = argv[1];
+    const char* DATABASE = argv[2];
+    const char* LOGIN = argv[3];
+    const char* PASSWORD = argv[4];
+    const char* UID = argv[5];
+    const char* MODEL = argv[6];
+    const char* METHOD = argv[7];
+
+    std::cout   << "URL: "      << URL          << '\n'
+                << "DATABASE: " << DATABASE     << '\n'
+                << "LOGIN: "    << LOGIN        << '\n'
+                << "PASSWORD: " << PASSWORD     << '\n'
+                << "UID: "      << UID          << '\n'
+                << "MODEL: "    << MODEL        << '\n'
+                << "METHOD: "   << METHOD       << '\n'
+    << std::endl;
+
+    size_t args_size = (size_t)(argc - 8);
+    std::vector<std::string> ARGS(args_size);
+    for(size_t index = 0; index < args_size; ++index)
+        ARGS[index] = argv[8 + index];
 
     // TODO: Verify if id is the session id return in the cookies
     OdooRPC client (
         URL,
         DATABASE,
         {
-            argv[3],
-            argv[4]
+            LOGIN,
+            PASSWORD
         }
     );
 
-    if(argc > 5) {
-        client.forceUid(argv[5]);
-        std::cout << client.raw_query(
-            "res.partner",
-            "search",
-            {
-                "[]"
-            }
-        ) << std::endl;
-    }
+    client.forceUid(UID);
+    std::cout << client.raw_query(
+        MODEL,
+        METHOD,
+        ARGS
+    ) << std::endl;
 
 
     return EXIT_SUCCESS;
